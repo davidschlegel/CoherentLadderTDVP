28c231396027e6c2ec3ea6f2c1ff1d4c41f4dc08
David Schlegel <david.schlegel@epfl.ch>
2023-12-05 10:45:05 +0100

    add ad2_a2 to SingleModeCoherentTDVP

--- singlemodeproblemtype.jl
+++ singlemodeproblemtype.jl
@@ -7,12 +7,13 @@ mutable struct SingleModeCoherentTDVP
     S⁻¹ ::Matrix{ComplexF64}
     L::Matrix{ComplexF64}
     a:: Matrix{ComplexF64}
+    ad:: Matrix{ComplexF64}
     ad_a:: Matrix{ComplexF64}
     a2:: Matrix{ComplexF64}
     C0el::Int
     polα::Transpose{ComplexF64, Vector{ComplexF64}}
     drift::Matrix{ComplexF64}
-    #ad2_a2:: Matrix{ComplexF64}
+    ad2_a2:: Matrix{ComplexF64}
     # Add any other parameters or fields necessary
 
     # Constructor
@@ -25,11 +26,13 @@ mutable struct SingleModeCoherentTDVP
         self.S⁻¹ = Matrix{ComplexF64}(undef, order, order)
         self.L = Matrix{ComplexF64}(undef, order+1, order)
         self.a = Matrix{ComplexF64}(undef, order+1, order)
+        self.ad = Matrix{ComplexF64}(undef, order+1, order)
         self.ad_a = Matrix{ComplexF64}(undef, order+1, order)
         self.a2 = Matrix{ComplexF64}(undef, order+1, order)
         self.C0el = factorial(order)
         self.polα = transpose(Vector{ComplexF64}(undef, order))
         self.drift = Matrix{ComplexF64}(undef, order, order)
+        self.ad2_a2 = Matrix{ComplexF64}(undef, order+1, order)
         # Initialize other fields as needed
         return self
     end
@@ -40,6 +43,7 @@ end
 function (prob::SingleModeCoherentTDVP)(du, u, p, t)
     α = u.x[1][1]
     B = u.x[2]
+    # println(t, "\t", α)
     # update S-matrixes
     smatrix!(prob.S, α)
     prob.S⁻¹ .= inv(Hermitian(@view(prob.S[1:end-1, :])))
@@ -51,15 +55,21 @@ function (prob::SingleModeCoherentTDVP)(du, u, p, t)
     polyα!(prob.polα, prob.order, α)
     #prob.polα .= transpose(polyα(prob.order, α))
 
-    trY = ((transpose(@view(prob.L[end, :])) - prob.polα *@view(prob.L[1:end-1, :]))*@view(B[:, end]))[1]
+    # Problem Here!
+    trY = (transpose(@view(prob.L[end, :])) - prob.polα *@view(prob.L[1:end-1, :]))*@view(B[:, end])
 
     trC = prob.C0el*(transpose(@view(B[end, :]))*@view(prob.S[1:end-1, :])*@view(B[:, end]))
 
     # derivative of α, including regularization
-    du.x[1][1] = (trY + 1e-16) / (trC + 1e-8)
+    # if abs(trY) < 1e-12
+    #     du.x[1][1] = 0
+    # else
+    #     du.x[1][1] = trY / trC
+    # end
+    du.x[1][1] = (trY + 1e-14) / (trC + 1e-8)
 
     # drift term, avoids inverse
-    prob.drift = B[:,end] * prob.polα 
+    prob.drift .= B[:,end] * prob.polα 
     prob.drift[:, 2:end] .+= B[:, 1:end-1] #size(B) == (1, 1) ? nothing : 
     prob.drift .*= conj(du.x[1][1])
 
